a3f1f211d24293a9797c267b80b78490767f7681
Manoj Sahu <manoj_sahu@outlook.com>
2023-05-20 16:48:57 +0530

    user entity for restful crud operations

---
+++ src/main/java/com/manoj/springboot/restful/springbootrestfulwebservice/entity/User.java
@@ -0,0 +1,33 @@
+package com.manoj.springboot.restful.springbootrestfulwebservice.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "user_data")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+    @Column(nullable = false, unique = true)
+    private String email;
+}
---
+++ src/test/java/com/manoj/springboot/restful/springbootrestfulwebservice/SpringbootRestfulWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manoj.springboot.restful.springbootrestfulwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRestfulWebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

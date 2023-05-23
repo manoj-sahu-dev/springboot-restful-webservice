67b8bb72ad590021f3de6b3324955b6e35ba6164
Manoj Sahu <manoj_sahu@outlook.com>
2023-05-23 23:20:29 +0530

    validate & custom exception

--- src/main/java/com/manoj/springboot/restful/springbootrestfulwebservice/controller/UserController.java
+++ src/main/java/com/manoj/springboot/restful/springbootrestfulwebservice/controller/UserController.java
@@ -2,6 +2,7 @@ package com.manoj.springboot.restful.springbootrestfulwebservice.controller;
 
 import com.manoj.springboot.restful.springbootrestfulwebservice.dto.UserDto;
 import com.manoj.springboot.restful.springbootrestfulwebservice.service.UserService;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,14 +17,14 @@ public class UserController {
     private UserService userService;
 
     @PostMapping
-    public ResponseEntity<UserDto> createUser(@RequestBody UserDto userDto) {
+    public ResponseEntity<UserDto> createUser(@RequestBody @Valid UserDto userDto) {
         UserDto savedUser = userService.createUser(userDto);
         return new ResponseEntity<UserDto>(savedUser, HttpStatus.CREATED);
 
     }
 
     @GetMapping("{id}")
-    public ResponseEntity<UserDto> getUserById(@PathVariable("id") Long id) {
+    public ResponseEntity<UserDto> getUserById(@PathVariable Long id) {
         UserDto user = userService.getUserById(id);
         return new ResponseEntity<UserDto>(user, HttpStatus.OK);
     }
@@ -36,13 +37,13 @@ public class UserController {
     }
 
     @PutMapping("update")
-    public ResponseEntity<UserDto> updateUser(@RequestBody UserDto user) {
+    public ResponseEntity<UserDto> updateUser(@RequestBody @Valid UserDto user) {
         UserDto updatedUser = userService.updateUser(user);
         return new ResponseEntity<UserDto>(updatedUser, HttpStatus.OK);
     }
 
     @DeleteMapping("delete/{id}")
-    public ResponseEntity<String> deleteUser(@PathVariable("id") Long id) {
+    public ResponseEntity<String> deleteUser(@PathVariable Long id) {
         userService.deleteUser(id);
         return new ResponseEntity<String>("user deleted", HttpStatus.OK);
 
--- src/main/java/com/manoj/springboot/restful/springbootrestfulwebservice/dto/UserDto.java
+++ src/main/java/com/manoj/springboot/restful/springbootrestfulwebservice/dto/UserDto.java
@@ -1,5 +1,7 @@
 package com.manoj.springboot.restful.springbootrestfulwebservice.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,7 +13,14 @@ import lombok.Setter;
 @AllArgsConstructor
 public class UserDto {
     private Long id;
-    private String firstName;
+
+    @NotEmpty(message = "users first name must not be empty or null")
+    private String firstName; // first name should not be null or empty
+
+    @NotEmpty(message = "users last name must not be empty or null")
     private String lastName;
-    private String email;
+
+    @NotEmpty(message = "users emailmust not be empty or null")
+    @Email(message = "users email must be valid")
+    private String email; // must be valid email
 }
